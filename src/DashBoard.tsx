2039ff2d6a6d99a519bb2861a5201c185d70833b
戸田優香 <homu65@gmail.com>
2024-11-06 17:54:18 +0900

    BarコンポーネントではなくChartコンポーネントを使う

--- src/DashBoard.tsx
+++ src/DashBoard.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Bar } from "react-chartjs-2";
+import { categoryAGraphData, categoryBGraphData } from "./GraphData";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -13,8 +12,10 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { categoryAGraphData, categoryBGraphData } from "./GraphData";
+import { Chart } from "react-chartjs-2";
+import { useState } from "react";
 
+// 必要な要素を登録
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -58,25 +59,15 @@ const DashBoard: React.FC = () => {
   const options = {
     responsive: true,
     scales: {
-      x: {
-        ticks: {
-          // 横軸のメモリの色
-          color: "black",
-        },
-      },
       y1: {
         type: "linear" as const,
         position: "left" as const,
         title: {
           display: true,
           text: "新規ユーザー数",
-          // 左軸のラベルの色
           color: "rgba(75, 192, 192, 0.6)",
         },
-        ticks: {
-          // 左軸の目盛りの色
-          color: "rgba(75, 192, 192, 0.6)",
-        },
+        ticks: { color: "rgba(75, 192, 192, 0.6)" },
       },
       y2: {
         type: "linear" as const,
@@ -84,16 +75,10 @@ const DashBoard: React.FC = () => {
         title: {
           display: true,
           text: "アクティブユーザー数",
-          // 右軸のラベルの色
           color: "rgba(255, 99, 132, 1)",
         },
-        ticks: {
-          // 右軸の目盛りの色
-          color: "rgba(255, 99, 132, 1)",
-        },
-        grid: {
-          drawOnChartArea: false,
-        },
+        ticks: { color: "rgba(255, 99, 132, 1)" },
+        grid: { drawOnChartArea: false },
       },
     },
     plugins: {
@@ -103,24 +88,22 @@ const DashBoard: React.FC = () => {
   };
 
   return (
-    <>
-      <div>
-        <div className="pb-3">
-          <label>
-            カテゴリを選択:
-            <select
-              className="bg-white border"
-              value={selectedCategory}
-              onChange={(e) => setSelectedCategory(e.target.value as "A" | "B")}
-            >
-              <option value="A">A</option>
-              <option value="B">B</option>
-            </select>
-          </label>
-        </div>
-        <Bar data={chartData} options={options} />
+    <div>
+      <div className="pb-4">
+        <label>
+          カテゴリを選択:
+          <select
+            className="bg-white border"
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value as "A" | "B")}
+          >
+            <option value="A">A</option>
+            <option value="B">B</option>
+          </select>
+        </label>
       </div>
-    </>
+      <Chart type="bar" data={chartData} options={options} />
+    </div>
   );
 };
 
